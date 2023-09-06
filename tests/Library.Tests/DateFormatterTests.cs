793554244d3c31bcfd099d9f6f30d2e76b0323e1
Ignacio Carbajal <ignaciocarbajal2002@gmail.com>
2023-09-06 10:18:00 -0300

    quedo andando la fecha correcta

--- tests/Library.Tests/DateFormatterTests.cs
+++ tests/Library.Tests/DateFormatterTests.cs
@@ -8,11 +8,11 @@ public class DataFormatterTests
     }
 
     [Test]
-    public void DateTests()
+    public void FechaCorrecta()
     {
         const string dateIngresado = "06/09/2023";
-        const string dateExpected = "2023-06-09";
-        const string dateFormateada = DataFormatterTests.ChangeFormat(date);
+        const string dateExpected = "2023-09-06";
+        string dateFormateada = TestDateFormat.DateFormatter.ChangeFormat(dateIngresado);
         Assert.AreEqual(dateFormateada, dateExpected);
     }
 }
